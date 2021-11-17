5d01f8de47c929d5e7716563ad685cf589108be5
savenkov <savenkov@cmd.su>
2021-11-17 16:00:36 +0300

    1st lesson

--- lesson-one/practice-routes.js
+++ lesson-one/practice-routes.js
@@ -3,6 +3,7 @@ function reqHandler(req, res) {
     const method = req.method;
 
     if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html lang="en">');
         res.write('<head><title>nodejs</title></head>');
         res.write('<body><h1>hello from "/"</h1><form action="/create-user" method="POST"><input name="username" type="text"></form></body>');
@@ -11,6 +12,7 @@ function reqHandler(req, res) {
     }
 
     if (url === '/users') {
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html lang="en">');
         res.write('<head><title>nodejs</title></head>');
         res.write('<ul><li>User 1</li></ul>');
@@ -20,15 +22,13 @@ function reqHandler(req, res) {
 
     if (url === '/create-user' && method === 'POST') {
         const body = [];
-        req.on('data', chunk => {
-            body.push(chunk);
-        });
+        req.on('data', chunk => body.push(chunk));
         req.on('end', () => {
             const parsedData = Buffer.concat(body).toString();
-            res.writeHead(302, { Location: '/users' });
-            return res.end();
+            console.log(parsedData.split('=')[1]);
         });
-
+        res.writeHead(302, { Location: '/users' });
+        res.end();
     }
 }
 
