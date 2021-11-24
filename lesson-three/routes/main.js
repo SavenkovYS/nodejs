937cb683559f45e8053793d2417a85c77d1ea4e0
Yura <savenkov@cmd.su>
2021-11-24 21:45:41 +0300

    adding routes

---
+++ lesson-three/routes/main.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+const rootDir = require('../utils/path');
+
+const path = require('path');
+
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'index.html'));
+});
+
+module.exports = router;
