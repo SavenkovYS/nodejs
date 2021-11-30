4a38dba69e61714fbea4a8653b17e9e73e1a20c4
savenkov <savenkov@cmd.su>
2021-11-30 16:53:03 +0300

    1st lesson

--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const shopController = require('../controllers/shop');
@@ -11,6 +13,8 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
 
 router.get('/orders', shopController.getOrders);
 
