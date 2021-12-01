f224b46b261d26e859a1e85e9def14579ebe9eda
savenkov <savenkov@cmd.su>
2021-12-01 16:59:44 +0300

    1st lesson

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-   host: 'localhost',
-   user: 'root',
-   database: 'node_compolete',
-   password: '1234'
+const sequelize = new Sequelize('node_compolete', 'root', '1234', {
+   dialect: 'mysql',
+   host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
