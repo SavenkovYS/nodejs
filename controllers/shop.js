02aa4bbb028ad71fdccff1804f318515c11fb140
savenkov <savenkov@cmd.su>
2021-12-03 17:01:48 +0300

    1st lesson

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,23 +2,32 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-      .then(([rows, fieldData]) => {
-          res.render('shop/product-list', {
-            prods: rows,
-            pageTitle: 'All Products',
-            path: '/products'
-          })
+  Product.findAll()
+      .then(products => {
+        res.render('shop/product-list', {
+          prods: products,
+          pageTitle: 'All Products',
+          path: '/products'
         })
-      .catch(err => console.log(err));;
+      })
+      .catch(err => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-      .then(([product]) => {
+  // Product.findAll({ where: {
+  //     id: prodId
+  //   }})
+  //     .then(products => res.render('shop/product-detail', {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: '/product'
+  //     }))
+  //     .catch(err => console.log(err));
+  Product.findByPk(prodId)
+      .then(product => {
         res.render('shop/product-detail', {
-          product: product[0],
+          product: product,
           pageTitle: product.title,
           path: '/product'
         })
@@ -27,10 +36,10 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-      .then(([rows, fieldData]) => {
+  Product.findAll()
+      .then(products => {
         res.render('shop/index', {
-          prods: rows,
+          prods: products,
           pageTitle: 'Shop',
           path: '/'
         })
@@ -39,32 +48,49 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.fetchAll(products => {
-      const cartProducts = [];
-      for (product of products) {
-        const cartProductData = cart.products.find(
-          prod => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts
-      });
-    });
-  });
+  req.user.getCart()
+      .then(cart => {
+        return cart.getProducts()
+            .then(products => {
+                res.render('shop/cart', {
+                  path: '/cart',
+                  pageTitle: 'Your Cart',
+                  products: products
+                });
+            })
+            .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err))
 };
 
 exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+    const prodId = req.body.productId;
+    let fetchedCart = null;
+    req.user
+        .getCart()
+        .then(cart => {
+            fetchedCart = cart;
+            return cart.getProducts({ where: { id: prodId } })
+        })
+        .then(products => {
+            let product = null;
+            if (products.length > 0) {
+                product = products[0];
+            }
+            let newQuantity = 1;
+            if (product) {
+
+            }
+            return Product.findByPk(prodId)
+                .then(product => {
+                    return fetchedCart.addProduct(product, { through: { quantity: newQuantity } });
+                })
+                .catch(err => console.log(err));
+        })
+        .then(() => {
+            res.redirect('/cart');
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node_compolete', 'root', '1234', {
+const sequelize = new Sequelize('node_complete', 'root', '1234', {
    dialect: 'mysql',
    host: 'localhost'
 });
