bbf9faf21691e1241019a954106e4424cca0fb73
savenkov <savenkov@cmd.su>
2021-12-06 14:43:57 +0300

    sql

--- util/database.js
+++ util/database.js
@@ -1,8 +1,38 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+//
+// const sequelize = new Sequelize('node_complete', 'root', '1234', {
+//    dialect: 'mysql',
+//    host: 'localhost'
+// });
+//
+// module.exports = sequelize;
 
-const sequelize = new Sequelize('node_complete', 'root', '1234', {
-   dialect: 'mysql',
-   host: 'localhost'
-});
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect = (callback) => {
+   MongoClient.connect('mongodb+srv://savenkov:Cfdtyrjd1993@cluster0.rrnoz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+       .then(client => {
+          console.log('Connected');
+          _db = client.db('shop');
+          callback();
+       })
+       .catch(err => {
+           console.log(err);
+           throw err;
+       });
+}
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
+
